7ce173460d6a8dbfddc0dfc21775a3bb401bab22
Aman <159995323+Aman-SecurityKnock@users.noreply.github.com>
2025-03-12 15:57:55 +0530

    Update scheduler.ps1

--- src/scheduler.ps1
+++ src/scheduler.ps1
@@ -1,7 +1,7 @@
 # scheduler.ps1
 # Define paths and URLs
 $scriptUrl = "https://raw.githubusercontent.com/Aman-SecurityKnock/ScreenCapture-TelegramBot/main/src/Combined2.ps1"
-$localScriptPath = "$env:TEMP\Combined2.ps1"
+$localScriptPath = "C:\Temp\Combined2.ps1"
 
 # Download the latest Combined2.ps1 from GitHub
 Invoke-WebRequest -Uri $scriptUrl -OutFile $localScriptPath -UseBasicParsing
